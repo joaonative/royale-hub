7ff8f5ade62e35489b9ed8a6fa6d603943bbc9b8
joaonative <jestudos127@gmail.com>
2024-09-13 16:36:29 -0300

    cards page fetching cards and sort

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,9 @@
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterModule, RouterOutlet],
-  providers: [],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -1,12 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ClashRoyaleService } from '../services/clash-royale.service';
+import { Card } from '../models/clash-royale.interfaces';
 
 @Component({
   selector: 'app-cards',
   standalone: true,
   imports: [],
   templateUrl: './cards.component.html',
-  styleUrl: './cards.component.css'
 })
-export class CardsComponent {
+export class CardsComponent implements OnInit {
+  constructor(private clashRoyaleService: ClashRoyaleService) {}
 
+  cards: Card[] = [];
+  sortOrder: 'asc' | 'desc' = 'desc';
+
+  ngOnInit(): void {
+    this.clashRoyaleService.getCards().subscribe(
+      (response) => {
+        this.cards = response.items;
+        this.sortCards();
+      },
+      (error) => {
+        console.error('Cards Method Error: ', error);
+      }
+    );
+  }
+
+  toggleSortOrder(): void {
+    this.sortOrder = this.sortOrder === 'desc' ? 'asc' : 'desc';
+    this.sortCards();
+  }
+
+  sortCards(): void {
+    this.cards.sort((a, b) => {
+      const rarityOrder = ['common', 'rare', 'epic', 'legendary'];
+      const aIndex = rarityOrder.indexOf(a.rarity);
+      const bIndex = rarityOrder.indexOf(b.rarity);
+      return this.sortOrder === 'desc' ? bIndex - aIndex : aIndex - bIndex;
+    });
+  }
+
+  getRarityColor(rarity: string): string {
+    switch (rarity) {
+      case 'common':
+        return '#3D7FFF';
+      case 'rare':
+        return '#ED7219';
+      case 'epic':
+        return '#9D12F3';
+      case 'legendary':
+        return '#0cc2cd';
+      default:
+        return 'black';
+    }
+  }
 }
