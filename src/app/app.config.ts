b9bf4f7de09cdb4061392e1de11b36aaa1ea8786
joaonative <jestudos127@gmail.com>
2024-09-16 18:24:22 -0300

    feat: ssr to call directly supercell api

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,12 +2,18 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withFetch,
+  withInterceptors,
+} from '@angular/common/http';
 import { authInterceptor } from './services/auth.interceptor';
+import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(withInterceptors([authInterceptor])),
+    provideHttpClient(withFetch(), withInterceptors([authInterceptor])),
+    provideClientHydration(),
   ],
 };
