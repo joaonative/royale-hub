0768c6bd8f6987de6ed9fb5413f881f4b346d7a7
joaonative <jestudos127@gmail.com>
2024-09-16 16:12:11 -0300

    feat: loading component and impl

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { ClashRoyaleService } from '../services/clash-royale.service';
 import { Card } from '../models/clash-royale.interfaces';
 import { CardComponent } from '../ui/card/card.component';
+import { LoadingComponent } from '../ui/loading/loading.component';
 
 @Component({
   selector: 'app-cards',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, LoadingComponent],
   templateUrl: './cards.component.html',
 })
 export class CardsComponent implements OnInit {
@@ -15,13 +16,17 @@ export class CardsComponent implements OnInit {
   cards: Card[] = [];
   sortOrder: 'asc' | 'desc' = 'desc';
 
+  loading: boolean = false;
+
   ngOnInit(): void {
+    this.loading = true;
     this.clashRoyaleService.getCards().subscribe({
       next: (res) => {
         this.cards = res.items;
         this.sortCards();
       },
       error: (err) => console.error('Get Cards Error: ', err),
+      complete: () => (this.loading = false),
     });
   }
 
--- src/app/clans/clans.component.ts
+++ src/app/clans/clans.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { ClashRoyaleService } from '../services/clash-royale.service';
 import { Clan } from '../models/clash-royale.interfaces';
 import { ClanCardComponent } from '../ui/clan-card/clan-card.component';
+import { LoadingComponent } from '../ui/loading/loading.component';
 
 @Component({
   selector: 'app-clans',
   standalone: true,
-  imports: [ClanCardComponent],
+  imports: [ClanCardComponent, LoadingComponent],
   templateUrl: './clans.component.html',
 })
 export class ClansComponent {
@@ -14,12 +15,16 @@ export class ClansComponent {
 
   featuredClans: Clan[] = [];
 
+  loading: boolean = false;
+
   ngOnInit(): void {
+    this.loading = true;
     this.clashRoyaleService.getFeaturedClans().subscribe({
       next: (res) => {
         (this.featuredClans = res.items), console.log(this.featuredClans);
       },
       error: (err) => console.error('Get FeaturedClans Error: ', err),
+      complete: () => (this.loading = false),
     });
   }
 }
