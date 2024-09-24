33c2432d89cd14c69f473dad515910163487aa4c
joaonative <jestudos127@gmail.com>
2024-09-24 13:43:57 -0300

    using standalone node server to request clash royale api

--- src/app/services/auth.interceptor.ts
+++ src/app/services/auth.interceptor.ts
@@ -4,9 +4,6 @@ import { environment } from '../../environments/environment';
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const authReq = req.clone({
     url: `${environment.apiUrl}${req.url}`,
-    setHeaders: {
-      Authorization: `Bearer ${environment.apiKey}`,
-    },
   });
 
   return next(authReq);
