f6a264b12031d5f79faa78ac21c26e65ff56a424
joaonative <jestudos127@gmail.com>
2024-09-24 14:43:06 -0300

    feat: battle logs on player details

--- src/app/player-details/player-details.component.ts
+++ src/app/player-details/player-details.component.ts
@@ -1,4 +1,4 @@
-import { UpcomingChests } from './../models/clash-royale.interfaces';
+import { UpcomingChests, BattleLog } from './../models/clash-royale.interfaces';
 import { Component } from '@angular/core';
 import { Player } from '../models/clash-royale.interfaces';
 import { ClashRoyaleService } from '../services/clash-royale.service';
@@ -13,6 +13,7 @@ import {
   TrophyIcon,
   ArrowUpDownIcon,
   SwordsIcon,
+  DropletIcon,
 } from 'lucide-angular';
 import { CardComponent } from '../ui/card/card.component';
 
@@ -27,11 +28,12 @@ export class PlayerDetailsComponent {
   readonly TrophyIcon = TrophyIcon;
   readonly ArrowUpDownIcon = ArrowUpDownIcon;
   readonly SwordsIcon = SwordsIcon;
+  readonly DropletIcon = DropletIcon;
 
   playerTag: string | null = null;
 
   player?: Player;
-
+  battleLogs: BattleLog[] = [];
   upcomingChests: UpcomingChests[] = [];
 
   badgeName: string = '';
@@ -54,14 +56,11 @@ export class PlayerDetailsComponent {
       this.clashRoyaleService.searchPlayer(this.playerTag).subscribe({
         next: (res) => {
           this.player = res;
-          const badge = badges.find((b) => b.id === this.player?.clan?.badgeId);
-          if (badge) {
-            this.badgeName = badge.name;
-          } else {
-            this.badgeName = 'Unknown Badge';
-          }
         },
         error: () => this.router.navigate(['/404']),
+      });
+      this.clashRoyaleService.getBattleLog(this.playerTag).subscribe({
+        next: (res) => (this.battleLogs = res),
       });
       this.clashRoyaleService.getUpcomingChests(this.playerTag).subscribe({
         next: (res) => {
@@ -96,5 +95,17 @@ export class PlayerDetailsComponent {
     }
 
     return formattedName;
+  }
+
+  getBadgeName(id: number | undefined): string {
+    if (!id) {
+      return 'no_clan';
+    }
+    const badge = badges.find((b) => b.id === id);
+    if (badge) {
+      return badge.name;
+    } else {
+      return 'no_clan';
+    }
   }
 }
